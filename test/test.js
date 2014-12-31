274d50467bc5d5587df377e40cb4881e707b377e
Justin Meyer <justinbmeyer@gmail.com>
2014-12-31 13:12:54 -0600

    tests can and jquery-ui

--- test/test.js
+++ test/test.js
@@ -54,32 +54,23 @@ asyncTest("module names that start with @", function(){
 	}).then(start);
 });
 
-/*asyncTest("Loads buildConfig", function(){
-	System.import("test/build_config/bower.json!bower").then(function(){
-		var config = System.buildConfig;
-		ok(config, "buildConfig added");
-		equal(config.map.foo, "bar", "Correct map included");
+asyncTest("jquery-ui", function(){
+	GlobalSystem.paths["@foo"] = "test/foo.js";
+	Promise.all([
+		GlobalSystem.import("jquery"),
+		GlobalSystem.import("jquery-ui/draggable")
+	]).then(function(mods){
+		var $ = mods[0];
+		ok($.fn.draggable);
 	}).then(start);
+
 });
 
-asyncTest("Replaces bower_components path in paths", function(){
-	System.bowerPath = "vendor";
-	System.import("test/alt_path/bower.json!bower").then(function(){
-		equal(System.paths.bar, "vendor/bar/bar.js", "Correct path set");
-	}).then(start);
-});*/
-
 // Only run these tests for StealJS (because it requires steal syntax)
-if(System.isSteal) {
-	asyncTest("Modules with their own config works", function(){
-		System.import("can").then(function(can){
-			var $ = can.$;
-			var tmpl = can.mustache("Hello {{name}}");
-			$("#qunit-test-area").html(tmpl({
-				name: "World"
-			}));
-
-			equal($("#qunit-test-area").html(), "Hello World", "Loaded can and rendered a template");
+if(window.steal) {
+	asyncTest("canjs", function(){
+		GlobalSystem.import("can/control/control").then(function(Control){
+			ok(Control.extend, "Control has an extend method");
 		}).then(start);
 	});
 }
