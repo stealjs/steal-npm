a7ea0118059b8712527e3bc0086a92b630351e15
Julian <julian@Julians-MacBook-Pro.local>
2016-05-29 21:09:45 +0200

    add tests for npmAlgorithm

--- test/helpers.js
+++ test/helpers.js
@@ -149,19 +149,20 @@ Runner.prototype.npmVersion = function(version){
 	if(arguments.length === 0) {
 		return this._version; 
 	}
-
-	this.algorithm = version >= 3 ? "flat": "nested";
 	this._version = version;
-	this._addVersion();
 	return this;
 };
 
 Runner.prototype._addVersion = function(){
 	var root = this.root;
-	var algo = this.algorithm;
-	if(algo) {
-		var system = root.system = root.system || {};
-		system.npmAlgorithm = algo;
+	var system = root.system = root.system || {};
+
+	if(root.system.npmAlgorithm === 'nested') {
+		system.npmAlgorithm = this.algorithm = 'nested';
+		this.npmVersion(2.15); // latest npm 2
+	} else {
+		system.npmAlgorithm = this.algorithm = 'flat';
+		this.npmVersion(3);
 	}
 };
 
